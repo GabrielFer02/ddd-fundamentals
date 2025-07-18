38edaa4160d6f702db65b993186038e9249a35e9
Gabriel Fernando <gf313074@gmail.com>
2025-07-18 20:34:29 -0300

    abstracted create entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,13 +1,29 @@
-import { Entity } from "../../core/entities/entity.js"
+import { Entity } from "../../core/entities/entity.js";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id.js";
+import { Optional } from "../../core/types/optional.js";
 
 interface AnswerProps {
-  content: string
-  authorId: string
-  questionId: string
+  content: string;
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps> {
   get content() {
-    return this.props.content
+    return this.props.content;
+  }
+
+  static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const answer = new Answer({
+      ...props,
+      createdAt: new Date(),
+    }, id)
+
+    return answer
   }
 }
