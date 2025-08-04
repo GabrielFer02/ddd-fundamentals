2bd39eda905953203a66ac931bfa8c8843ad6079
Gabriel Fernando <gf313074@gmail.com>
2025-08-03 22:14:03 -0300

    create delete answer use case and test

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -0,0 +1,44 @@
+import { makeAnswerComment } from 'test/factories/make-answer-comment.js';
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository.js';
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.js';
+import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment.js';
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: DeleteAnswerCommentUseCase;
+
+describe('Delete Answer Comment', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository);
+  });
+
+  it('should be able to delete a answer comment', async () => {
+    const answerComment = makeAnswerComment();
+
+    await inMemoryAnswerCommentsRepository.create(answerComment);
+
+    await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: answerComment.authorId.toString(),
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
+  });
+
+  it('should not be able to delete another user answer comment', async () => {
+    const answerComment = makeAnswerComment({
+      authorId: new UniqueEntityID('author-1'),
+    });
+
+    await inMemoryAnswerCommentsRepository.create(answerComment);
+
+    await expect(() =>
+      sut.execute({
+        answerCommentId: answerComment.id.toString(),
+        authorId: 'author-2',
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
