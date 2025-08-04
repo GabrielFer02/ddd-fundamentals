d4c922b53d4ca75c397844d1d71d653cb3b29b0c
Gabriel Fernando <gf313074@gmail.com>
2025-08-03 21:59:05 -0300

    create delete question comment use case and test

---
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -0,0 +1,35 @@
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository.js';
+
+interface DeleteQuestionCommentUseCaseRequest {
+  authorId: string;
+  questionCommentId: string;
+}
+
+interface DeleteQuestionCommentUseCaseResponse {
+  [key: string]: string;
+}
+
+export class DeleteQuestionCommentUseCase {
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    authorId,
+    questionCommentId,
+  }: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
+    const questionComment = await this.questionCommentsRepository.findById(
+      questionCommentId,
+    );
+
+    if (!questionComment) {
+      throw new Error('Question comment not found.');
+    }
+
+    if (questionComment.authorId.toString() !== authorId) {
+      throw new Error('Not allowed');
+    }
+
+    await this.questionCommentsRepository.delete(questionComment);
+
+    return {};
+  }
+}
