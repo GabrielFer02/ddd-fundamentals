2fe605a213f8e4e28c8e594ec00d2a3be3cac62e
Gabriel Fernando <gf313074@gmail.com>
2025-08-24 20:37:46 -0300

    create and delete question with attachments use cases

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository.js';
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository.js';
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id.js';
@@ -5,11 +6,16 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id.js';
 import { CreateQuestionUseCase } from './create-question.js';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
 let sut: CreateQuestionUseCase;
 
 describe('Create Question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository();
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    );
     sut = new CreateQuestionUseCase(inMemoryQuestionsRepository);
   });
 
